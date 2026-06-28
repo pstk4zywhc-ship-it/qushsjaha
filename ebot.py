d5f245c815a666af2ebf572b70894a30fd9972f7
pstk4zywhc-ship-it <pstk4zywhc@privaterelay.appleid.com>
2026-06-28 23:40:17 +0300

    Update ebot.py

--- ebot.py
+++ ebot.py
@@ -1,10 +1,11 @@
 import telebot
 from telebot import types
+import base64
 
 # توكن البوت الخاص بك
 bot = telebot.TeleBot("8704063502:AAFkLjIbI2MuM2dk9rY0d7qP-yaav4w-w-w")
 
-# تم ربط الرابط الخاص بك هنا
+# رابط صفحتك
 BASE_URL = "https://pstk4zywhc-ship-it.github.io/qushsjaha/"
 
 @bot.message_handler(commands=['start'])
@@ -21,9 +22,12 @@ def callback_query(call):
         bot.register_next_step_handler(call.message, get_text)
 
 def get_text(message):
-    user_text = message.text
-    # ربط النص بالرابط
-    final_link = f"{BASE_URL}?msg={user_text}"
+    # تشفير النص
+    user_text_bytes = message.text.encode('utf-8')
+    encoded_text = base64.b64encode(user_text_bytes).decode('utf-8')
+    
+    # الرابط الجديد
+    final_link = f"{BASE_URL}?msg={encoded_text}"
     bot.send_message(message.chat.id, f"تم إنشاء الرابط:\n{final_link}")
 
 if __name__ == "__main__":
